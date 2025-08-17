1ecfc1cdde1a43a10eb1df5b5d26db1fac8964ed
Eugene van der Merwe <eugene@vander.host>
2025-08-17 19:24:09 +0200

    Update unsubscribe.blade.php
    Add an extra check to see if the user has a UUID, because if the email was never sent, they won't have one (no subscription has been created).

--- resources/views/components/unsubscribe.blade.php
+++ resources/views/components/unsubscribe.blade.php
@@ -1,5 +1,7 @@
 @props(['subscription'])
 
+@if($subscription?->uuid)
 <div style="text-align: center; margin: 1em 0; font-size: 0.9em;">
     <a href="{{ route('notifications.unsubscribe', ['uuid' => $subscription->uuid]) }}">Unsubscribe</a>
 </div>
+@endif
